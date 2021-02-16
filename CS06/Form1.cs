e22e76d522d28139ec59ea199321617bc6058534
久文 西原 <mikan.sup.all@gmail.com>
2021-02-16 11:09:52 +0900

    first commit

---
+++ CS06/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+
+namespace CS06
+{
+    public partial class Form1 : Form
+    {
+        private int _value = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int GetValue()
+        {
+            _value++;
+
+            // メソッドで型を指定したら必ず値を返さないといけない
+            return 1;
+        }
+
+        // メッセージを表示させるだけなど、値を返さない場合はvoidを指定する
+        private void SetValue()
+        {
+            MessageBox.Show("Hello World");
+        }
+    }
+}
