b5fdd7e38b2846cd9ddb33d2ee1cd4912508f5b4
久文 西原 <mikan.sup.all@gmail.com>
2021-02-18 23:39:46 +0900

    update

--- CS07/Form1.cs
+++ CS07/Form1.cs
@@ -12,9 +12,27 @@ namespace CS07
 {
     public partial class Form1 : Form
     {
+        // ボタンをクリックしたときに値を記憶させる
+        private int _count = 0;
+
+        // 定数
+        private const int AAA = 10;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // ボタン2のクリックイベント
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(_count.ToString());
+        }
+
+        // ボタンクリックのイベント
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _count += 1;
         }
     }
 }
